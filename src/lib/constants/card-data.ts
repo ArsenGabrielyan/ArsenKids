b9bd454c784bf176f68fb0a4ee77a93eafd134b9
Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2025-10-12 21:10:07 +0400

    Changes in the 'Let's play' Button

--- src/lib/constants/card-data.ts
+++ src/lib/constants/card-data.ts
@@ -22,7 +22,7 @@ export const SERVICES: ICard<"service">[] = [
           type: "colors"
      },
      {
-          link: "/games",
+          link: "/games#main-games",
           type: "games"
      },
 ]
